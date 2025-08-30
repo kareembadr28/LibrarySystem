bc99d8884bf3f75cbb802d6896842f3e78e8d243
kareembadr28 <bdrkrym558@gmail.com>
2025-08-30 15:54:16 +0200

    add hasher helper

---
+++ LibrarySystem/Helpers/Hasher.cs
@@ -0,0 +1,16 @@
+﻿namespace LibrarySystem.Helpers
+{
+    public class Hasher
+    {
+        private const int Degree = 12;
+        public static string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, Degree);
+        }
+
+        public static bool Verify(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}
